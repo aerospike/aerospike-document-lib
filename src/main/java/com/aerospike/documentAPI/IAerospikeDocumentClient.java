6abe650bf4ea4a71cc57f42c7b747011f145ad27
yrizhkov <yrizhkov@aerospike.com>
2021-06-02 16:39:44 +0300

    fix raw use of parameterized classes

--- src/main/java/com/aerospike/documentAPI/IAerospikeDocumentClient.java
+++ src/main/java/com/aerospike/documentAPI/IAerospikeDocumentClient.java
@@ -8,8 +8,9 @@ public interface IAerospikeDocumentClient {
 
     /**
      * Retrieve the object in the document with key documentKey that is referenced by the JSON path.
+     *
      * @param documentKey An Aerospike Key.
-     * @param jsonPath A JSON path to get the reference from.
+     * @param jsonPath    A JSON path to get the reference from.
      * @return Object referenced by jsonPath.
      */
     Object get(Key documentKey, String jsonPath)
@@ -17,33 +18,38 @@ public interface IAerospikeDocumentClient {
 
     /**
      * Put a document.
+     *
      * @param documentKey An Aerospike Key.
-     * @param jsonObject A JSON object to put.
+     * @param jsonObject  A JSON object to put.
      */
-    void put(Key documentKey, Map jsonObject);
+    void put(Key documentKey, Map<?, ?> jsonObject);
 
     /**
      * Put a map representation of a JSON object at a particular path in a JSON document.
+     *
      * @param documentKey An Aerospike Key.
-     * @param jsonPath A JSON path to put the given JSON object in.
-     * @param jsonObject A JSON object to put in the given JSON path.
+     * @param jsonPath    A JSON path to put the given JSON object in.
+     * @param jsonObject  A JSON object to put in the given JSON path.
      */
     void put(Key documentKey, String jsonPath, Object jsonObject)
             throws JsonPathParser.JsonParseException, AerospikeDocumentClient.AerospikeDocumentClientException;
 
     /**
      * Append an object to a list in a document specified by a JSON path.
+     *
      * @param documentKey An Aerospike Key.
-     * @param jsonPath A JSON path that includes a list to append the given JSON object to.
-     * @param jsonObject A JSON object to append to the list at the given JSON path.
+     * @param jsonPath    A JSON path that includes a list to append the given JSON object to.
+     * @param jsonObject  A JSON object to append to the list at the given JSON path.
      */
     void append(Key documentKey, String jsonPath, Object jsonObject)
             throws JsonPathParser.JsonParseException, AerospikeDocumentClient.AerospikeDocumentClientException;
 
     /**
      * Delete an object in a document specified by a JSON path.
+     *
      * @param documentKey An Aerospike Key.
-     * @param jsonPath A JSON path for the object deletion.
+     * @param jsonPath    A JSON path for the object deletion.
      */
-    void delete(Key documentKey, String jsonPath) throws JsonPathParser.JsonParseException, AerospikeDocumentClient.AerospikeDocumentClientException;
+    void delete(Key documentKey, String jsonPath)
+            throws JsonPathParser.JsonParseException, AerospikeDocumentClient.AerospikeDocumentClientException;
 }
--- src/test/java/com/aerospike/documentAPI/TestUtils.java
+++ src/test/java/com/aerospike/documentAPI/TestUtils.java
@@ -3,7 +3,13 @@ package com.aerospike.documentAPI;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 public class TestUtils {
 
@@ -11,11 +17,12 @@ public class TestUtils {
      * Determine whether two JSON objects are equivalent -
      * by serializing maps and list in a specific way and then testing serializations to see if they are equal.
      */
+    @SuppressWarnings("unchecked")
     public static boolean jsonEquals(Object object1, Object object2) {
-        if ((object1 instanceof Map) && (object2 instanceof Map)) {
-            return mapEquals((Map)object1,(Map)object2);
-        } else if ((object1 instanceof List) && (object2 instanceof List)) {
-            return listEquals((List)object1,(List)object2);
+        if ((object1 instanceof Map<?, ?>) && (object2 instanceof Map<?, ?>)) {
+            return mapEquals((Map<String, Object>) object1, (Map<String, Object>) object2);
+        } else if ((object1 instanceof List<?>) && (object2 instanceof List<?>)) {
+            return listEquals((List<?>) object1, (List<?>) object2);
         } else {
             return object1.toString().equals(object2.toString());
         }
@@ -29,23 +36,24 @@ public class TestUtils {
         HashMap<String, Object> map1 = new HashMap<>();
         HashMap<String, Object> map2 = new HashMap<>();
 
-        map1.put("key1",1);
-        map1.put("key2",2);
-        map1.put("key3",3);
-        map2.put("key3",3);
-        map2.put("key1",1);
-        map2.put("key2",2);
+        map1.put("key1", 1);
+        map1.put("key2", 2);
+        map1.put("key3", 3);
+        map2.put("key3", 3);
+        map2.put("key1", 1);
+        map2.put("key2", 2);
         Assert.assertTrue(TestUtils.jsonEquals(map1, map2));
     }
 
     /**
      * Generic JSON to string method.
      */
+    @SuppressWarnings("unchecked")
     private static String jsonToString(Object o) {
-        if (o instanceof Map) {
-            return jsonMapToString((Map<String, Object>)o);
-        } else if (o instanceof List) {
-            return jsonListToString((List)o);
+        if (o instanceof Map<?, ?>) {
+            return jsonMapToString((Map<String, Object>) o);
+        } else if (o instanceof List<?>) {
+            return jsonListToString((List<?>) o);
         } else if (o instanceof String) {
             return String.format("\"%s\"", o);
         } else {
@@ -57,7 +65,7 @@ public class TestUtils {
      * Serialization of JSON map.
      */
     private static String jsonMapToString(Map<String, Object> map) {
-        List<String> orderedKeys = new ArrayList(map.keySet());
+        List<String> orderedKeys = new ArrayList<>(map.keySet());
         Collections.sort(orderedKeys);
         Iterator<String> keyIterator = orderedKeys.iterator();
         List<String> stringRepresentations = new Vector<>();
@@ -73,9 +81,9 @@ public class TestUtils {
     /**
      * Serialization of JSON list.
      */
-    private static String jsonListToString(List list) {
-        Iterator listIterator = list.listIterator();
-        List<String> stringRepresentations = new Vector<>();
+    private static String jsonListToString(List<?> list) {
+        Iterator<?> listIterator = list.listIterator();
+        List<String> stringRepresentations = new ArrayList<>();
 
         while (listIterator.hasNext()) {
             stringRepresentations.add(jsonToString(listIterator.next()));
@@ -86,14 +94,14 @@ public class TestUtils {
     /**
      * Determine whether two JSON maps are equivalent.
      */
-    private static boolean mapEquals(Map map1, Map map2) {
+    private static boolean mapEquals(Map<String, Object> map1, Map<String, Object> map2) {
         return jsonMapToString(map1).equals(jsonMapToString(map2));
     }
 
     /**
      * Determine whether two JSON lists are equivalent.
      */
-    private static boolean listEquals(List list1, List list2) {
+    private static boolean listEquals(List<?> list1, List<?> list2) {
         return jsonListToString(list1).equals(jsonListToString(list2));
     }
 }
