4ef64081ec4f4f88967061d45f5b5296da70147f
roimenashe <roimenashe5@gmail.com>
2021-06-16 11:12:30 +0300

    Add methods with policies to the aerospike document client interface.

--- src/main/java/com/aerospike/documentapi/IAerospikeDocumentClient.java
+++ src/main/java/com/aerospike/documentapi/IAerospikeDocumentClient.java
@@ -1,6 +1,8 @@
 package com.aerospike.documentapi;
 
 import com.aerospike.client.Key;
+import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.WritePolicy;
 
 import java.util.Map;
 
@@ -17,12 +19,32 @@ public interface IAerospikeDocumentClient {
             throws JsonPathParser.JsonParseException, DocumentApiException;
 
     /**
+     * Retrieve the object in the document with key documentKey that is referenced by the JSON path.
+     *
+     * @param readPolicy  An Aerospike read policy to use for the get() operation.
+     * @param documentKey An Aerospike Key.
+     * @param jsonPath    A JSON path to get the reference from.
+     * @return Object referenced by jsonPath.
+     */
+    Object get(Policy readPolicy, Key documentKey, String jsonPath)
+            throws JsonPathParser.JsonParseException, DocumentApiException;
+
+    /**
      * Put a document.
      *
      * @param documentKey An Aerospike Key.
      * @param jsonObject  A JSON object to put.
      */
     void put(Key documentKey, Map<?, ?> jsonObject);
+
+    /**
+     * Put a document.
+     *
+     * @param writePolicy An Aerospike write policy to use for the put() operation.
+     * @param documentKey An Aerospike Key.
+     * @param jsonObject  A JSON object to put.
+     */
+    void put(WritePolicy writePolicy, Key documentKey, Map<?, ?> jsonObject);
 
     /**
      * Put a map representation of a JSON object at a particular path in a JSON document.
@@ -32,6 +54,17 @@ public interface IAerospikeDocumentClient {
      * @param jsonObject  A JSON object to put in the given JSON path.
      */
     void put(Key documentKey, String jsonPath, Object jsonObject)
+            throws JsonPathParser.JsonParseException, DocumentApiException;
+
+    /**
+     * Put a map representation of a JSON object at a particular path in a JSON document.
+     *
+     * @param writePolicy An Aerospike write policy to use for the put() and operate() operations.
+     * @param documentKey An Aerospike Key.
+     * @param jsonPath    A JSON path to put the given JSON object in.
+     * @param jsonObject  A JSON object to put in the given JSON path.
+     */
+    void put(WritePolicy writePolicy, Key documentKey, String jsonPath, Object jsonObject)
             throws JsonPathParser.JsonParseException, DocumentApiException;
 
     /**
@@ -45,11 +78,32 @@ public interface IAerospikeDocumentClient {
             throws JsonPathParser.JsonParseException, DocumentApiException;
 
     /**
+     * Append an object to a list in a document specified by a JSON path.
+     *
+     * @param writePolicy An Aerospike write policy to use for the operate() operation.
+     * @param documentKey An Aerospike Key.
+     * @param jsonPath    A JSON path that includes a list to append the given JSON object to.
+     * @param jsonObject  A JSON object to append to the list at the given JSON path.
+     */
+    void append(WritePolicy writePolicy, Key documentKey, String jsonPath, Object jsonObject)
+            throws JsonPathParser.JsonParseException, DocumentApiException;
+
+    /**
      * Delete an object in a document specified by a JSON path.
      *
      * @param documentKey An Aerospike Key.
      * @param jsonPath    A JSON path for the object deletion.
      */
     void delete(Key documentKey, String jsonPath)
+            throws JsonPathParser.JsonParseException, DocumentApiException;
+
+    /**
+     * Delete an object in a document specified by a JSON path.
+     *
+     * @param writePolicy An Aerospike write policy to use for the operate() operation.
+     * @param documentKey An Aerospike Key.
+     * @param jsonPath    A JSON path for the object deletion.
+     */
+    void delete(WritePolicy writePolicy, Key documentKey, String jsonPath)
             throws JsonPathParser.JsonParseException, DocumentApiException;
 }
