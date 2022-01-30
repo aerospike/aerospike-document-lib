80a395fbddb5d530118370abf8c413069fefe643
roimenashe <roimenashe5@gmail.com>
2022-01-30 13:25:13 +0200

    1. Handle null results. 2. Add unit test that checks for null results/exceptions.

---
+++ src/test/java/com/aerospike/documentapi/NullResultsTests.java
@@ -0,0 +1,60 @@
+package com.aerospike.documentapi;
+
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Key;
+import com.aerospike.client.exp.Exp;
+import com.aerospike.client.policy.Policy;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class NullResultsTests extends BaseTestConfig {
+
+    @Test
+    public void testNullResultsWithFilterExpression() throws IOException, JsonPathParser.JsonParseException, DocumentApiException {
+        AerospikeDocumentClient docClient = new AerospikeDocumentClient(client);
+
+        String jsonString = "{\"k1\":\"v1\", \"k2\":[1,2,3], \"k3\":[\"v31\", \"v32\", \"v34\"]}";
+        //Convert json string to json node
+        JsonNode jsonNode = JsonConverters.convertStringToJsonNode(jsonString);
+        //Record Key object
+        Key doc1 = new Key("test", "docset", "doc1");
+        //Insert document to DB
+        docClient.put(doc1, "docBin", jsonNode);
+
+        Map<String, List<Integer>> a = new HashMap<>();
+
+        List<Integer> jsonStringList = new Vector<>();
+        jsonStringList.add(1);
+        jsonStringList.add(2);
+        jsonStringList.add(3);
+        jsonStringList.add(4);
+        jsonStringList.add(5);
+
+        a.put("x1", jsonStringList);
+
+        docClient.put(doc1, "docBin", "$.k4", a);
+
+        Policy readPolicy = client.getReadPolicyDefault();
+        readPolicy.filterExp = Exp.build(Exp.eq(Exp.stringBin("docBin"), Exp.val("hi")));
+        readPolicy.failOnFilteredOut = false;
+
+        Object result = docClient.get(readPolicy, doc1, "docBin", "$.k4");
+        assertNull(result);
+
+        readPolicy.failOnFilteredOut = true;
+        try {
+            docClient.get(readPolicy, doc1, "docBin", "$.k4");
+            fail("Should fail with AerospikeException: Transaction filtered out.");
+        } catch (AerospikeException ignored) {
+        }
+    }
+}
