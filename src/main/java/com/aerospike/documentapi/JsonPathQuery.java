9286dbd22a940b67672d7da5bc4559f553b7ffb2
yrizhkov <yrizhkov@aerospike.com>
2022-08-19 19:05:36 +0300

    support JsonPath put operations on non-existent key

--- src/main/java/com/aerospike/documentapi/JsonPathQuery.java
+++ src/main/java/com/aerospike/documentapi/JsonPathQuery.java
@@ -2,6 +2,7 @@ package com.aerospike.documentapi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 
 public class JsonPathQuery {
 
@@ -14,7 +15,13 @@ public class JsonPathQuery {
     public static Object set(JsonPathObject jsonPathObject, Object object, Object value) throws JsonProcessingException {
         String resultJson = JsonConverters.convertObjectToJsonString(object);
         String jsonPath = "$" + jsonPathObject.getJsonPathSecondStepQuery();
-        return JsonPath.parse(resultJson).set(jsonPath, value).json();
+        JSONArray keys = JsonPath.parse(resultJson).read(jsonPath);
+        if (!keys.isEmpty()) {
+            return JsonPath.parse(resultJson).set(jsonPath, value).json();
+        }
+        String key = jsonPath.substring(jsonPath.lastIndexOf(".") + 1);
+        jsonPath = jsonPath.substring(0, jsonPath.lastIndexOf("."));
+        return JsonPath.parse(resultJson).put(jsonPath, key, value).json();
     }
 
     public static Object append(JsonPathObject jsonPathObject, Object object, Object value) throws JsonProcessingException {
